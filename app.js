7f57e554801904ed839fd720d8daca4353c3abfa
HuyToan <Toandh@mageplaza.com>
2021-12-07 17:33:29 +0700

    Uri's commit

--- app.js
+++ app.js
@@ -13,48 +13,86 @@ app.use('/user', userRoute)
 
 async function getUserInfo() {
     try {
-        const user = (await axios.get('https://jsonplaceholder.typicode.com/users')).data;
+        const users = (await axios.get('https://jsonplaceholder.typicode.com/users')).data;
         const posts = (await axios.get('https://jsonplaceholder.typicode.com/posts')).data;
         const comments = (await axios.get('https://jsonplaceholder.typicode.com/comments')).data;
 
-        const userData = [];
-        for (let i = 0; i < user.length; i++) {
-            let data = {
-                id: user[i].id,
-                name: user[i].name,
-                username: user[i].username,
-                email: user[i].email,
-                comments: [],
-                posts: []
-            }
-
-            for (let j = 0; j < posts.length; j++) {
-                if (user[i].id === posts[j].userId) {
-                    let post = {
-                        id: posts[j].postId,
-                        title: posts[j].title,
-                        body: posts[j].body
-                    }
-
-                    for (let k = 0; k < comments.length; k++) {
-                        if (posts[j].id === comments[k].postId) {
-                            let comment = {
-                                id: comments[k].id,
-                                name: comments[k].name,
-                                body: comments[k].body
-                            }
-                            data.comments.push(comment);
-                        }
-                    }
-
-                    data.posts.push(post);
+        const userData = users.map(user => {
+            const postData = posts.map(post => {
+                if (user.id === post.userId) {
+                    return {
+                        id: post.id,
+                        title: post.title,
+                        body: post.body
+                    };
                 }
+            });
+
+            if (postData) {
+                console.log(postData);
             }
 
-            userData.push(data);
-        }
+            // const commentData = comments.map(postData => {
+            //     if (postData.id === comments.postId) {
+            //         return {
+            //             id: comments.id,
+            //             postId: comments.postId,
+            //             name: comments.name,
+            //             body: comments.body
+            //         }
+            //     }
+            // });
 
-        console.error(userData);
+            return {
+                id: user.id,
+                name: user.name,
+                username: user.username,
+                email: user.email,
+                // comments: commentData,
+                posts: postData
+            }
+        });
+
+        // console.log(userData);
+
+        // const userData = [];
+        // for (let i = 0; i < user.length; i++) {
+        //     let data = {
+        //         id: user[i].id,
+        //         name: user[i].name,
+        //         username: user[i].username,
+        //         email: user[i].email,
+        //         comments: [],
+        //         posts: []
+        //     }
+        //
+        //     for (let j = 0; j < posts.length; j++) {
+        //         if (user[i].id === posts[j].userId) {
+        //             let post = {
+        //                 id: posts[j].postId,
+        //                 title: posts[j].title,
+        //                 body: posts[j].body
+        //             }
+        //
+        //             for (let k = 0; k < comments.length; k++) {
+        //                 if (posts[j].id === comments[k].postId) {
+        //                     let comment = {
+        //                         id: comments[k].id,
+        //                         name: comments[k].name,
+        //                         body: comments[k].body
+        //                     }
+        //                     data.comments.push(comment);
+        //                 }
+        //             }
+        //
+        //             data.posts.push(post);
+        //         }
+        //     }
+        //
+        //     userData.push(data);
+        // }
+        //
+        // console.error(userData);
     } catch (error) {
         console.error(error);
     }
--- basic/hello.js
+++ basic/hello.js
@@ -1,136 +1,194 @@
-// const myTechStack = ['NodeJs', 'ReactJs', 'Firebase']; // It is recommended that you use camelcase for variable name
-// const myTechStatus = myTechStack.map(stack => {
-//     return {
-//         name: stack,
-//         status: 'not-finished'
-//     }
-// });
-// console.log(myTechStatus);
+// // const myTechStack = ['NodeJs', 'ReactJs', 'Firebase']; // It is recommended that you use camelcase for variable name
+// // const myTechStatus = myTechStack.map(stack => {
+// //     return {
+// //         name: stack,
+// //         status: 'not-finished'
+// //     }
+// // });
+// // console.log(myTechStatus);
+// //
+// // // Filter only unique item
+// // const list = [11, 24, 31, 24, 11, 56, 34];
+// // const onlyUnique = (value, index, self) => {
+// //     return self.indexOf(value) === index;
+// // }
+// // const filterList = list.filter(onlyUnique);
+// // console.log(filterList);
+// //
 //
-// // Filter only unique item
-// const list = [11, 24, 31, 24, 11, 56, 34];
-// const onlyUnique = (value, index, self) => {
-//     return self.indexOf(value) === index;
-// }
-// const filterList = list.filter(onlyUnique);
-// console.log(filterList);
+// // //--------------------------------------------
+// // const getUser = () => {
+// //     // Mimic the process that we may retrieved the user data from the web somewhere
+// //     const user = {
+// //         id: 1,
+// //         name: "John Doe",
+// //         status: 'active'
+// //     };
+// //     // return the user object
+// //     return user;
+// // };
 //
-
+// // const {name, status} = getUser(); // You can use DA to extract only needed info
+// // console.log(name, status);
+// //
+// // const logInfo = ({name, age = 19}) => { // default argument with age = 19
+// //     console.log(`Hi, I'm ${name}. I'm ${age}`);
+// //     return `Hello, I'm ${name}. I'm ${age}`;
+// // };
+// // // With destruction parameter, you can swap the parameters order as you like
+// // console.log(logInfo({age: 19, name: "John Doe"}));
+//
+//
 // //--------------------------------------------
-// const getUser = () => {
-//     // Mimic the process that we may retrieved the user data from the web somewhere
-//     const user = {
-//         id: 1,
-//         name: "John Doe",
-//         status: 'active'
-//     };
-//     // return the user object
-//     return user;
-// };
-
-// const {name, status} = getUser(); // You can use DA to extract only needed info
-// console.log(name, status);
+// // const person = {
+// //     id: 1,
+// //     name: "John Doe",
+// // };
+// //
+// // const job = {job: 'NodeJs Developer'};
+// // const mergePerson = {...person, ...job};
+// // console.log(mergePerson);
+// //
+// // // You can use this to pick/omit fields from object
+// // const post = {
+// //     "userId": 1,
+// //     "id": 1,
+// //     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+// //     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+// // };
+// //
+// // const {userId, id, ...otherInfo} = post; // post object should contain only title and body
+// // console.log(post);
 //
-// const logInfo = ({name, age = 19}) => { // default argument with age = 19
-//     console.log(`Hi, I'm ${name}. I'm ${age}`);
-//     return `Hello, I'm ${name}. I'm ${age}`;
-// };
-// // With destruction parameter, you can swap the parameters order as you like
-// console.log(logInfo({age: 19, name: "John Doe"}));
-
-
-//--------------------------------------------
-// const person = {
-//     id: 1,
-//     name: "John Doe",
-// };
 //
-// const job = {job: 'NodeJs Developer'};
-// const mergePerson = {...person, ...job};
-// console.log(mergePerson);
+// //----------------------------------------------
+// // const delay = ms => new Promise(res => setTimeout(res, ms));
+// //
+// // const promise1 = async () => {
+// //     await delay(1000);
+// //     console.log("Promise 1 executed")
+// // };
+// //
+// // const promise2 = async () => {
+// //     await delay(3000)
+// //     console.log("Promise 2 executed")
+// // };
+// //
+// // // Consider this to be a controllers
+// // (async () => {
+// //     try {
+// //         const start = new Date();
+// //         await Promise.all([
+// //             promise2(),
+// //             promise1()
+// //         ]);
+// //         const end = new Date() - start
+// //         console.info('Execution time: %dms', end) // This will take 3s only
+// //     } catch (e){
+// //         console.log("Got an error here")
+// //     }
+// // })();
 //
-// // You can use this to pick/omit fields from object
-// const post = {
-//     "userId": 1,
-//     "id": 1,
-//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-//     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-// };
 //
-// const {userId, id, ...otherInfo} = post; // post object should contain only title and body
-// console.log(post);
-
-
-//----------------------------------------------
-// const delay = ms => new Promise(res => setTimeout(res, ms));
+// //--------------------------------------------------
+// // async function hello() {
+// //     return await Promise.resolve("Hello"); //await sẽ tạm dừng mã cho đến khi Promise hoàn thành
+// // };
+// //
+// // // hello().then((value) => console.log(value))
+// // hello().then(console.log);
 //
-// const promise1 = async () => {
-//     await delay(1000);
-//     console.log("Promise 1 executed")
-// };
+// // //-------------------------------------------------- //Callback and callback hell
+// // function getData(callback) {
+// //     setTimeout(() => {
+// //         const data = [
+// //             {id: 1, name: "Developer A"},
+// //             {id: 2, name: "Devloper B"}
+// //         ]; // Assume the data from API
+// //
+// //         callback(data); //Callback là cách đảm bảo code sẽ không hoạt động trước khi các code khác hoàn thành việc thực thi.
+// //                         //Không có data thì hàm k hàm k hoạt động tại các function khác gọi đến
+// //     }, 1000)
+// // }
+// // //
+// // // getData((data) => {
+// // //     const names = data.map(data => data.id);
+// // //     console.log(typeof(names));
+// // // });
+// //
+// // /**
+// //  * Fake like we are submitting the data to an API elsewhere
+// //  *
+// //  * @param inputData
+// //  * @param callback
+// //  */
+// // function submitData (inputData, callback) {
+// //     console.log('starting to submit');
+// //     setTimeout(() => {
+// //         // Act like we submit the inputData to the server and takes 1s
+// //         const status = randomStatus();
+// //         callback({
+// //             success: status
+// //         });
+// //     }, 1000)
+// // }
+// //
+// // /**
+// //  * Just a helper random the result of the submit: success or failed
+// //  *
+// //  * @returns {boolean}
+// //  */
+// // const randomStatus = () => [true, false][Math.floor(Math.random() * 2)];
+// //
+// // // Main body of the example
+// // getData((data) => {
+// //     const names = data.map(data => data.name);
+// //     console.log(names)
+// //     submitData(names, (response) => {
+// //         const {success} =  response;
+// //         if (success === true) {
+// //             console.log("This is a successful form")
+// //         } else {
+// //             console.log("This is a failed form")
+// //         }
+// //     })
+// // });
 //
-// const promise2 = async () => {
-//     await delay(3000)
-//     console.log("Promise 2 executed")
-// };
-//
-// // Consider this to be a controllers
-// (async () => {
-//     try {
-//         const start = new Date();
-//         await Promise.all([
-//             promise2(),
-//             promise1()
-//         ]);
-//         const end = new Date() - start
-//         console.info('Execution time: %dms', end) // This will take 3s only
-//     } catch (e){
-//         console.log("Got an error here")
-//     }
-// })();
-
-
-//--------------------------------------------------
-// async function hello() {
-//     return await Promise.resolve("Hello"); //await sẽ tạm dừng mã cho đến khi Promise hoàn thành
-// };
-//
-// // hello().then((value) => console.log(value))
-// hello().then(console.log);
-
-// //-------------------------------------------------- //Callback and callback hell
-// function getData(callback) {
-//     setTimeout(() => {
+// //----------------------------------------Promise
+// /**
+//  * Asynchronous programming with Promise
+//  */
+// function getData() {
+//     return new Promise((resolve, reject) => {
 //         const data = [
 //             {id: 1, name: "Developer A"},
 //             {id: 2, name: "Devloper B"}
-//         ]; // Assume the data from API
+//         ];
 //
-//         callback(data); //Callback là cách đảm bảo code sẽ không hoạt động trước khi các code khác hoàn thành việc thực thi.
-//                         //Không có data thì hàm k hàm k hoạt động tại các function khác gọi đến
-//     }, 1000)
+//         setTimeout(() => {
+//             resolve(data);
+//         }, 1000);
+//     });
 // }
-// //
-// // getData((data) => {
-// //     const names = data.map(data => data.id);
-// //     console.log(typeof(names));
-// // });
 //
 // /**
 //  * Fake like we are submitting the data to an API elsewhere
 //  *
 //  * @param inputData
-//  * @param callback
 //  */
-// function submitData (inputData, callback) {
+// function submitData (inputData) {
 //     console.log('starting to submit');
-//     setTimeout(() => {
+//
+//     return new Promise((resolve, reject) => {
 //         // Act like we submit the inputData to the server and takes 1s
 //         const status = randomStatus();
-//         callback({
-//             success: status
-//         });
-//     }, 1000)
+//
+//         setTimeout(() => {
+//             resolve({
+//                 success: status
+//             });
+//         }, 1000);
+//     });
 // }
 //
 // /**
@@ -141,87 +199,37 @@
 // const randomStatus = () => [true, false][Math.floor(Math.random() * 2)];
 //
 // // Main body of the example
-// getData((data) => {
-//     const names = data.map(data => data.name);
-//     console.log(names)
-//     submitData(names, (response) => {
-//         const {success} =  response;
-//         if (success === true) {
-//             console.log("This is a successful form")
-//         } else {
-//             console.log("This is a failed form")
-//         }
-//     })
-// });
-
-//----------------------------------------Promise
-/**
- * Asynchronous programming with Promise
- */
-function getData() {
-    return new Promise((resolve, reject) => {
-        const data = [
-            {id: 1, name: "Developer A"},
-            {id: 2, name: "Devloper B"}
-        ];
-
-        setTimeout(() => {
-            resolve(data);
-        }, 1000);
-    });
-}
-
-/**
- * Fake like we are submitting the data to an API elsewhere
- *
- * @param inputData
- */
-function submitData (inputData) {
-    console.log('starting to submit');
-
-    return new Promise((resolve, reject) => {
-        // Act like we submit the inputData to the server and takes 1s
-        const status = randomStatus();
-
-        setTimeout(() => {
-            resolve({
-                success: status
-            });
-        }, 1000);
-    });
-}
-
-/**
- * Just a helper random the result of the submit: success or failed
- *
- * @returns {boolean}
- */
-const randomStatus = () => [true, false][Math.floor(Math.random() * 2)];
-
-// Main body of the example
-// getData().then(data => {
+// // getData().then(data => {
+// //     const names = data.map(data => data.name);
+// //     console.log(names);
+// //     submitData(names).then(response => {
+// //         const {success} =  response;
+// //         if (success === true) {
+// //             console.log("This is a successful form")
+// //         } else {
+// //             console.log("This is a failed form")
+// //         }
+// //     });
+// // });
+//
+// (async () => {
+//     const data = await getData();
 //     const names = data.map(data => data.name);
 //     console.log(names);
-//     submitData(names).then(response => {
-//         const {success} =  response;
-//         if (success === true) {
-//             console.log("This is a successful form")
-//         } else {
-//             console.log("This is a failed form")
-//         }
-//     });
-// });
-
-(async () => {
-    const data = await getData();
-    const names = data.map(data => data.name);
-    console.log(names);
-    const {success} = await submitData(names);
-    if (success === true) {
-        console.log("This is a successful form")
-    } else {
-        console.log("This is a failed form")
+//     const {success} = await submitData(names);
+//     if (success === true) {
+//         console.log("This is a successful form")
+//     } else {
+//         console.log("This is a failed form")
+//     }
+// })();
+//
+//
+const myTechStack = ['NodeJs', 'ReactJs', 'Firebase']; // It is recommended that you use camelcase for variable name
+const myTechStatus = myTechStack.map(stack => {
+    return {
+        name: stack,
+        status: 'not-finished'
     }
-})();
-
-
+});
+console.log(myTechStatus)
